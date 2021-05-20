26c77ff94be074866e5c7d719102222479194077
Bala Ramadurai <bala@balaramadurai.net>
2021-05-20 11:39:56 +0530

    Update interface_config.js
    participants-pane - is this an option?

--- docs/interface_config.js
+++ docs/interface_config.js
@@ -33,7 +33,7 @@ var interfaceConfig = {
 //    "etherpad",
 //    "sharedvideo",
     "settings",
-    "participants",
+    "participants-pane",
     "raisehand",
     "videoquality",
     "filmstrip",
