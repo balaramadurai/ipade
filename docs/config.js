6d714e3cffbc0887e8c7845c1a5971ccbae4e8c6
balaramadurai <bala@balaramadurai.net>
2021-05-23 12:53:50 +0530

    pade.chat to jitsi.br

--- docs/config.js
+++ docs/config.js
@@ -20,7 +20,7 @@ var config = {
       "max": 1080
     }
   }},
-  "hiddenDomain": "recorder.pade.chat",
+  "hiddenDomain": "recorder.jitsi.balaramadurai.net",
   "videoQuality": {
     "minHeightForQualityLvl": {
       "720": "high",
@@ -36,14 +36,14 @@ var config = {
   "enableLipSync": false,
   "minHDHeight": 540,
   "enableLayerSuspension": true,
-  "websocket": "wss://pade.chat:5443/ws/",
+  "websocket": "wss://jitsi.balaramadurai.net:5443/ws/",
   "startAudioMuted": 9,
   "channelLastN": -1,
   "adaptiveLastN": false,
   "hosts": {
-    "domain": "pade.chat",
-    "focus": "focus.pade.chat",
-    "muc": "conference.pade.chat"
+    "domain": "jitsi.balaramadurai.net",
+    "focus": "focus.jitsi.balaramadurai.net",
+    "muc": "conference.jitsi.balaramadurai.net"
   },
   "testing": {
     "capScreenshareBitrate": 1,
@@ -63,7 +63,7 @@ var config = {
   "maxFullResolutionParticipants": -1,
   "useStunTurn": false,
   "openBridgeChannel": "websocket",
-  "bosh": "https://pade.chat:5443/http-bind/",
+  "bosh": "https://jitsi.balaramadurai.net:5443/http-bind/",
   "enableNoAudioDetection": true,
   "defaultLanguage": "en",
   "useRoomAsSharedDocumentName": false,
@@ -93,6 +93,6 @@ var config = {
   "disableSimulcast": true,
   "startAudioOnly": false,
   "ofmeetLiveStream": false,
-  "focusUserJid": "focus@pade.chat",
+  "focusUserJid": "focus@jitsi.balaramadurai.net",
   "enableRemb": true
 };
